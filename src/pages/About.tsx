131b21b13d260a5bfa1a01d86d4ea180f79987c6
Josué Loredo Cortes <loredo.josue@hotmail.com>
2025-05-20 00:14:54 -0600

    Actualización

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 export default function About() {
   return (
     <>
-      <div className="bg-[url('/images/landing/bg-2.jpg')] bg-cover bg-center h-screen">
+      <div className="bg-[url('/images/landing/bg-2.jpg')] bg-cover bg-center min-h-screen mx-10">
         <NavBar />
       </div>
     </>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 export default function Contact() {
   return (
     <>
-      <div className="bg-[url('/images/landing/bg-3.jpg')] bg-cover bg-center h-screen">
+      <div className="bg-[url('/images/landing/bg-3.jpg')] bg-cover bg-center min-h-screen mx-10">
         <NavBar />
       </div>
     </>
